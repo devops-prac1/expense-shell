414dcb92a835958f2b0d131bb575577fe17f8c90
dspriyasg <priya9.sdr@gmail.com>
2024-03-05 20:02:16 +0800

    Adding check status to all scripts

--- frontend.sh
+++ frontend.sh
@@ -2,32 +2,32 @@ source common.sh
 
 
 print_Task_Heading "Install Nginx"
-dnf install nginx -y  &>>&LOG
+dnf install nginx -y  &>>$LOG
 check_status $?
 
 
 print_Task_Heading "copy expense nginx configuration"
-cp expense.conf /etc/nginx/default.d/expense.conf &>>&LOG
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$LOG
 check_status $?
 
 
 
 print_Task_Heading "clean old content"
-rm -rf /usr/share/nginx/html/* &>>&LOG
+rm -rf /usr/share/nginx/html/* &>>$LOG
 check_status $?
 
 
 
 print_Task_Heading "download app content"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>&LOG
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>$LOG
 check_status $?
 
 print_Task_Heading "Extract app content"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>&LOG
+unzip /tmp/frontend.zip &>>$LOG
 check_status $?
 
 print_Task_Heading "start  Nginx service"
-systemctl enable nginx &>>&LOG
-systemctl restart nginx &>>&LOG
+systemctl enable nginx &>>$LOG
+systemctl restart nginx &>>$LOG
 check_status $?
