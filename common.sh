4543d50511fc407740e7b94ffc76aac634584510
dspriyasg <priya9.sdr@gmail.com>
2024-03-05 16:16:00 +0800

    giving the status of every operation whether its success or failure

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@ print_Task_Heading(){
   echo "########   $1 ################" &>>/tmp/expense.log
 }
 check_status() {
-  if [ $1 -eq 0]; then
+  if [ $1 -eq 0]: then
     echo success
   else
     echo failure
