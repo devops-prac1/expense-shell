7c393444bca9e9af37e69e4586fddcecacca8347
dspriyasg <priya9.sdr@gmail.com>
2024-03-05 14:03:43 +0800

    addressing the password

--- backend.sh
+++ backend.sh
@@ -1,4 +1,7 @@
+mysql_root_password=$1
+
 echo Disable default NodeJS Version Module
+
 dnf module disable nodejs -y  &>>/tmp/expense.log
 
 echo enable NodeJS  Module for V20
@@ -24,4 +27,4 @@ systemctl daemon-reload  &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
 dnf install mysql -y  &>>/tmp/expense.log
-mysql -h 172.31.16.209 -uroot -pExpenseApp@1 < /app/schema/backend.sql  &>>/tmp/expense.log
+mysql -h 172.31.16.209 -uroot -p${mysql_root_password} < /app/schema/backend.sql  &>>/tmp/expense.log
