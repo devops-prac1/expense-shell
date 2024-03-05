2f3e66030d39b100517ad63aaa0c2584a6893df9
dspriyasg <priya9.sdr@gmail.com>
2024-03-05 16:14:09 +0800

    giving the status of every operation whether its success or failure

--- backend.sh
+++ backend.sh
@@ -13,108 +13,57 @@ fi
 
  print_Task_Heading "Disable default NodeJS Version Module"
 dnf module disable nodejs -y  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
+
 
 print_Task_Heading  "enable NodeJS  Module for V20"
 dnf module enable nodejs:20 -y  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "Install NodeJS"
 dnf install nodejs -y  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "Adding Application user"
 useradd expense &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "copy backend service file"
 cp backend.service /etc/systemd/system/backend.service  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "clean the old content"
 rm -rf /app  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "create App Directory"
 mkdir /app  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "Download app content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "extract app content"
 cd /app  &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading  "Download NodeJS Dependencies"
 cd /app  &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading  "start backend service"
 systemctl daemon-reload  &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "install MYSQL client"
 dnf install mysql -y  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
 
 print_Task_Heading "Load schema"
 mysql -h 172.31.16.209 -uroot -p${mysql_root_password} < /app/schema/backend.sql  &>>/tmp/expense.log
-if [ $? -eq 0]; then
-  echo sucess
-else
-  echo Failure
-fi
+check_status $?
