83b36586c28946cba7e2a74bb3a5633b810ed2d9
dspriyasg <priya9.sdr@gmail.com>
2024-03-05 19:20:41 +0800

    given color code

--- common.sh
+++ common.sh
@@ -4,8 +4,8 @@ print_Task_Heading(){
 }
 check_status() {
   if [ $1 -eq 0 ]; then
-    echo -e "\e[32msuccess\e[0m"
+    echo -e "\e[32m success \e[0m"
   else
-    echo -e "\e[31mfailure\e[0m"
+    echo -e "\e[31m failure \e[0m"
   fi
 }
